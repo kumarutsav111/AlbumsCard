65de56ea5c0ed3a1d0d19ba03ceeb467bd010c22
EVRY <evry@EVRYs-Mac-mini.local>
2019-04-11 12:11:45 +0530

    Add existing file

--- App.js
+++ App.js
@@ -6,30 +6,32 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Image, ScrollView, Linking } from 'react-native';
 import axios from 'axios';
+import Card from './components/Card'
+import Button from './components/Button'
 
 type Props = {};
 export default class App extends Component<Props> {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       responseData: []
     }
   }
-  componentDidMount(){
+  componentDidMount() {
     var axios = require('axios');
 
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-    .then(response => {
-      this.setState({responseData: response.data})
-    })
-    .catch(error => {
-      console.error(error);
-      
-    })
-    
+      .then(response => {
+        this.setState({ responseData: response.data })
+      })
+      .catch(error => {
+        console.error(error);
+
+      })
+
     // axios.all([
     //   axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2017-08-03'),
     //   axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2017-08-02')
@@ -54,50 +56,88 @@ export default class App extends Component<Props> {
     // .catch((error) => {
     //   console.error(error);      
     // })
-//     const axios = require('axios');
-// axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-//   .then(response => {
-//     console.log(response.data.url);
-//     console.log(response.data.explanation);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
+    //     const axios = require('axios');
+    // axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    //   .then(response => {
+    //     console.log(response.data.url);
+    //     console.log(response.data.explanation);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
   }
   lapsList() {
     return this.state.responseData.map(albums => {
+      const { title, artist, image, thumbnail_image , url} = albums;
+      const { thumbnailStyle,
+        headerContent,
+        thumbnailContainerStyle,
+        headerTextStyle,
+        imageStyle
+      } = styles;
       return (
-        <View><Text>{albums.title}</Text></View>
+        <Card>
+          <View>
+            <View style={thumbnailContainerStyle}>
+              <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+            </View>
+            <View style={headerContent} >
+              <Text style={headerTextStyle}>{title}</Text>
+              <Text>{artist}</Text>
+            </View>
+          </View>
+          <View>
+            <Image
+              style={imageStyle}
+              source={{ uri: image }}
+            />
+          </View>
+          <Button onPress = {() => Linking.openURL(url)}>
+            Buy Now!!!
+          </Button>
+        </Card >
       )
     })
-}
+  }
   render() {
-    
+
     return (
-      <View style={styles.container}>
+      <ScrollView >
         {this.lapsList()}
-      </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  headerContent: {
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    width: 50,
+    height: 50,
+  },
+  imageStyle: {
+    height: 400,
     flex: 1,
+    width: null
+  },
+  thumbnailContainerStyle: {
     justifyContent: 'center',
     alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 // state = {
 //   data: ''
